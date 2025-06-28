5a4a9e3d1456bdb17c5810135298c64e2f47bc03
Bhanu Prakash <131232954+prakash2670@users.noreply.github.com>
2025-06-28 21:23:50 +0530

    Update 4

--- main.js
+++ main.js
@@ -1,21 +1,71 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 const { spawn } = require('child_process');
 
+let tray = null;
+let mainWindow;
+let isQuiting = false;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 900,
     height: 600,
+    icon: path.join(__dirname, 'icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   });
 
-  win.loadFile('index.html');
+  mainWindow.loadFile('index.html');
+
+  // Minimize to tray
+  mainWindow.on('minimize', function (event) {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  // Hide to tray on close
+  mainWindow.on('close', function (event) {
+    if (!isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+}
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        if (mainWindow) {
+          mainWindow.show();
+        }
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        isQuiting = true;
+        app.quit();
+      }
+    }
+  ]);
+  tray.setToolTip('LocalTap - Realtime Tracker');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('double-click', () => {
+    mainWindow.show();
+  });
 }
 
 app.whenReady().then(() => {
+  createWindow();
+  createTray();
+
+  // Start background servers
   spawn('python', ['server.py'], {
     cwd: __dirname,
     detached: true,
@@ -27,6 +77,8 @@ app.whenReady().then(() => {
     detached: true,
     stdio: 'ignore'
   });
+});
 
-  createWindow();
+app.on('before-quit', () => {
+  isQuiting = true;
 });
