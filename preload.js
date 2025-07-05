2c0edaf296fa6d95ccc7f05f4f96da017368de31
Bhanu Prakash <131232954+prakash2670@users.noreply.github.com>
2025-07-05 19:00:26 +0530

    Cleared Complete Electron Debris

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    onUpdateTopKeys: (callback) => ipcRenderer.on('update-top-keys', (event, data) => callback(data))
-});
